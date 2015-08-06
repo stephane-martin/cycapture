866d917e0059344d2717b67697e3b97bc65e0638
stephane-martin <stephane.martin_github@vesperal.eu>
2015-08-07 00:11:27 +0200

    update imports

--- cycapture/libtins/__init__.py
+++ cycapture/libtins/__init__.py
@@ -6,7 +6,7 @@ from .exceptions import LibtinsException, MalformedAddress, MalformedPacket, Mal
 from .exceptions import OptionPayloadTooLarge, FieldNotPresent, PDUNotFound, InvalidInterface, UnknownLinkType
 from .exceptions import SocketOpenError, SocketCloseError, SocketWriteError, InvalidSocketType
 from .exceptions import BadTinsCast, ProtocolDisabled
-from ._tins import IPv4Address, HWAddress, NetworkInterface, PDU, EthernetII, IP, TCP, Raw, IPv4Range, UDP, DNS
+from ._tins import IPv4Address, IPv6Address, HWAddress, NetworkInterface, IPv4Range, IPv6Range, HWRange
+from ._tins import PDU, EthernetII, IP, TCP, Raw, IPv4Range, UDP, DNS
 from ._tins import factory_PDU_from_typed_memoryview
-
 
