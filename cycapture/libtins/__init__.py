543178a2a8275f895c44583b2ca3e435b3f8b23f
stephane-martin <stephane.martin_github@vesperal.eu>
2015-08-12 00:40:05 +0200

    add DNS PDU

--- cycapture/libtins/__init__.py
+++ cycapture/libtins/__init__.py
@@ -19,7 +19,8 @@ from ._tins import IPv4Address, IPv6Address, HWAddress, IPv4Range, IPv6Range, HW
 from ._tins import PDU, factory_PDU_from_typed_memoryview
 
 # concrete PDUs
-from ._tins import EthernetII, IP, TCP, UDP, Raw, DNS
+from ._tins import EthernetII, IP, TCP, UDP, Raw
+from ._tins import DNS, DNS_Query, DNS_Resource
 
 # utils
 from ._tins import RouteEntry, get_route_entries, list_network_interfaces, pdutype_to_string
