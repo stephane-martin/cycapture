26d845141b1dedca1081c3f3b45a3a23bc8586de
stephane-martin <stephane.martin_github@vesperal.eu>
2015-08-14 01:24:52 +0200

    custom exceptions

--- cycapture/libpcap/__init__.py
+++ cycapture/libpcap/__init__.py
@@ -4,5 +4,9 @@
 libpcap bindings using cython
 """
 
+from .exceptions import PcapException, AlreadyActivated, SetTimeoutError, SetDirectionError, SetBufferSizeError
+from .exceptions import SetSnapshotLengthError, SetPromiscModeError, SetMonitorModeError, SetNonblockingModeError
+from .exceptions import ActivationError, NotActivatedError, SniffingError
+
 from ._pcap import Sniffer
 from ._pcap import get_pcap_version, lookupdev, findalldevs, lookupnet, get_pcap_version
