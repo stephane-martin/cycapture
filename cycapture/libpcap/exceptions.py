fcd1e0e083ea07e43e81ad23032e24dbaa1db5c5
stephane-martin <stephane.martin_github@vesperal.eu>
2015-08-14 01:24:46 +0200

    custom exceptions

---
+++ cycapture/libpcap/exceptions.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'stef'
+
+class PcapException(Exception):
+    pass
+
+class AlreadyActivated(PcapException):
+    pass
+
+class SetTimeoutError(PcapException):
+    pass
+
+class SetDirectionError(PcapException):
+    pass
+
+class SetBufferSizeError(PcapException):
+    pass
+
+class SetSnapshotLengthError(PcapException):
+    pass
+
+class SetPromiscModeError(PcapException):
+    pass
+
+class SetMonitorModeError(PcapException):
+    pass
+
+class SetNonblockingModeError(PcapException):
+    pass
+
+class ActivationError(PcapException):
+    pass
+
+class NotActivatedError(PcapException):
+    pass
+
+class SniffingError(PcapException):
+    pass
