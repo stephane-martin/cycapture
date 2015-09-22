f28fe013ed845b07b63ba084e205a06a56af6016
stephane-martin <stephane.martin_github@vesperal.eu>
2015-09-22 18:52:07 +0200

    rewrite without externs

--- cycapture/libtins/custom_exception_handler.cpp
+++ cycapture/libtins/custom_exception_handler.cpp
@@ -3,25 +3,27 @@
 #include <stdexcept>
 #include <ios>
 #include "Python.h"
-// #include "libtins_exceptions.h"
+#include "_py_exceptions.h"
 #include "tins/exceptions.h"
 #include "custom_exception_handler.h"
 
+
 // the objects will be loaded at runtime
-extern PyObject *py_libtins_exception;
-extern PyObject *py_option_not_found;
-extern PyObject *py_malformed_packet;
-extern PyObject *py_pdu_not_found;
-extern PyObject *py_invalid_interface;
-extern PyObject *py_field_not_present;
-extern PyObject *py_socket_open_error;
-extern PyObject *py_socket_close_error;
-extern PyObject *py_socket_write_error;
-extern PyObject *py_invalid_socket_type;
-extern PyObject *py_unknown_link_type;
-extern PyObject *py_malformed_option;
-extern PyObject *py_bad_tins_cast;
-extern PyObject *py_protocol_disabled;
+//extern PyObject *py_libtins_exception;
+//extern PyObject *py_option_not_found;
+//extern PyObject *py_malformed_packet;
+//extern PyObject *py_pdu_not_found;
+//extern PyObject *py_invalid_interface;
+//extern PyObject *py_field_not_present;
+//extern PyObject *py_socket_open_error;
+//extern PyObject *py_socket_close_error;
+//extern PyObject *py_socket_write_error;
+//extern PyObject *py_invalid_socket_type;
+//extern PyObject *py_unknown_link_type;
+//extern PyObject *py_malformed_option;
+//extern PyObject *py_bad_tins_cast;
+//extern PyObject *py_protocol_disabled;
+
 // option_payload_too_large seems not present in libtins 3.2
 //    extern PyObject *option_payload_too_large;
 
@@ -32,10 +34,12 @@ void custom_exception_handler() {
     // Catch a handful of different errors here and turn them into the
     // equivalent Python errors.
     try {
-        if (PyErr_Occurred())
+        if (PyErr_Occurred()) {
             ; // let the latest Python exn pass through and ignore the current one
-        else
+        }
+        else {
             throw;
+        }
     } catch (const Tins::option_not_found& exn) {
         PyErr_SetString(py_option_not_found, exn.what());
     } catch (const Tins::malformed_packet& exn) {
